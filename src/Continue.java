24e92bce73db1453dcca7a53118853560c173ce7
user <user@FH10>
2016-10-14 09:43:07 +0800

    OOP-HW3

---
+++ src/Continue.java
@@ -0,0 +1,13 @@
+
+public class Continue {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i ++) {	
+			if(i==0) continue;
+			else if(i==2) continue;
+			else if(i==4) continue;
+			System.out.println(i);
+				}
+			}
+	
+}
--- src/StringComparator.java
+++ src/StringComparator.java
@@ -4,18 +4,22 @@ public class StringComparator {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		
-		System.out.println("Enter a string 1:");
-		String str1 = scn.next();
-		System.out.println("Enter a string 2:");
-		String str2 = scn.next();
-		
-		if (str1.equalsIgnoreCase(str2)){
-			System.out.println("The two strings are the same.");
-		}
-		else{
-			System.out.println("The two strings are not the same.");
-		}
+		boolean check=true;
+		do{		
+			System.out.println("Enter a string 1:");
+			String str1 = scn.next();
+			System.out.println("Enter a string 2:");
+			String str2 = scn.next();
+			
+			if (str1.equalsIgnoreCase(str2)){
+				System.out.println("The two strings are the same.");
+				check=false;
+			}
+			else{
+				System.out.println("The two strings are not the same.");
+			}
+		}while(check);
+			
 	}
 
 }
